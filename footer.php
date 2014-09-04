6ecc4835ea8922f8245dae59ac6d3ac2c4c79d04
Ana Lu <luczynaki@gmail.com>
2014-09-04 14:22:27 -0500

    tracking code added

--- footer.php
+++ footer.php
@@ -27,5 +27,16 @@
 	wp_footer();
 ?>
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/prism.js"></script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-36317954-3', 'auto');
+  ga('send', 'pageview');
+
+</script>
 	</body>
 </html>
